ced632decaa14dcd1c52bb8f9ff740b5e65aabd6
Yotzin <yotzincastrejon@yahoo.com>
2021-03-17 08:31:58 -0700

    Initial Commit

---
+++ Yeehaw/YeehawApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeehawApp.swift
+//  Yeehaw
+//
+//  Created by Yotzin Castrejon on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct YeehawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
